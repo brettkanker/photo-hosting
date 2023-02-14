0be4934de9317173bae2547537c353dbd54a86a9
brettkanker <stanisic.luka44@gmail.com>
2023-02-14 15:52:52 +0100

    init

--- photohosting/src/main/java/ch/emvs/photohosting/PhotoHostingApplication.java
+++ photohosting/src/main/java/ch/emvs/photohosting/PhotoHostingApplication.java
@@ -1,13 +1,27 @@
 package ch.emvs.photohosting;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import ch.emvs.photohosting.storage.StorageProperties;
+import ch.emvs.photohosting.storage.StorageService;
 
 @SpringBootApplication
-public class PhotoHostingApplication {
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PhotoHostingApplication.class, args);
+		SpringApplication.run(UploadingFilesApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
