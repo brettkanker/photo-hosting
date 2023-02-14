f8148d5dfd44e3f2c2d27705b47b6411a04d51f0
brettkanker <stanisic.luka44@gmail.com>
2023-02-14 14:43:20 +0100

    init

---
+++ photohosting/src/test/java/ch/emvs/photohosting/PhotoHostingApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.emvs.photohosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoHostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
